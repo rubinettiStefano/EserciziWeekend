aae305f9e5fa943540d25d62ff113c9355424585
alieksieyeva <ira.allekseeva@gmail.com>
2023-12-01 15:11:57 +0100

    con esercizio Book

--- src/com/generation/es2/Book.java
+++ src/com/generation/es2/Book.java
@@ -1,5 +1,7 @@
 package com.generation.es2;
 
 public class Book {
-    
+    //un oggetto di tipo Book rappresenta un libro nella libreria del quartiere
+    //ha come proprietà titolo, nome dell'autore, genere, numero di pagine, anno pubblicazione e prezzo
+    //come metodo vogliamo toString()
 }
--- src/com/generation/es2/BookAvvio.java
+++ src/com/generation/es2/BookAvvio.java
@@ -4,6 +4,9 @@ public class BookAvvio
 {
     public static void main(String[] args) 
     {
-        
+        //Vorremmo un programma che crea tanti libri quanti desidera l'utente, raccogliendo i dati da quest'ultimo.
+        //il programma deve stampare toString per ogni libro ogni volta che ne viene creato uno.
+        //come output finale vorremmo il numero complessivo di quelli che sono stati pubblicati prima del 2000 e dopo il 2000 con corrispondenti prezzi totali di tutti i libri
+        //inoltre vorremmo che il programma sappia calcolare il prezzo medio per i libri usciti prima del 2000 e per quelli dopo (è un calcolo da fare fuori dall'iterazione.)
     }    
 }
