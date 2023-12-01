09b334c97b914084e1073df821992d427ae62ec8
alieksieyeva <ira.allekseeva@gmail.com>
2023-12-01 14:53:41 +0100

    con esercizio Studente

---
+++ src/com/generation/es1/Student.java
@@ -0,0 +1,7 @@
+//Creare classe tipo Student
+//un oggetto di tipo Student ha come proprietà nome, cognome, sezione, 
+//voto per educazione fisica, per matematica, per inglese, per italiano, per storia, per fisica
+//creare un costruttore che permette di istanziare oggetto di tipo Student impostando le propietà ai valori passati come parametri
+//un oggetto di tipo Student può calcolare (ha i metodi per calcolare) la media e il numero di debiti
+//può produrre una rappresentazione testuale di tutte le informazioni riguadanti il singolo oggetto(metodo toString()).
+
