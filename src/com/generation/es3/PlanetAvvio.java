17ce7fac88f96e3360353457c5eb815da143010e
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-12-01 15:11:57 +0100

    planet

--- src/com/generation/es3/PlanetAvvio.java
+++ src/com/generation/es3/PlanetAvvio.java
@@ -2,7 +2,18 @@ package com.generation.es3;
 
 public class PlanetAvvio 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        //Abstract: vogliamo fare un po di statistica su alcuni pianeti inseriti
+
+        //Chiedere all'astronomo quanti pianeti vuole inserire
+        //Il programma dovrà calcolare QUANTI pianeti abitabili ci sono a meno
+        //di 100 anni luce dalla terra  e quanti pianeti "rocky" ci sono in totale
+
+        //Nel ciclo chiedere tutti i dati all'astronomo, far stampare il pianeta
+        //e fare i calcoli del caso
+
+        //stampare i risultati delle domande sopra
         
     }    
 }
