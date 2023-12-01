e11bbc16b511efed360fccca0e19326fd375e097
rubinettiStefano <stefano.rubinetti@gmail.com>
2023-12-01 15:24:17 +0100

    v finale

--- src/com/generation/es4/Smartphone.java
+++ src/com/generation/es4/Smartphone.java
@@ -1,5 +1,21 @@
 package com.generation.es4;
 
-public class Smartphone {
+/**
+ * Uno smartphone ha le seguenti proprietà:
+ * marca
+ * modello
+ * sistemaOperativo: android o iOs
+ * anno uscita
+ * prezzo
+ * dimensioniSchermo: (in pollici)
+ * 
+ * possiede 2 metodi
+ * il classico toString
+ * un metodo che ci dice se è contraffatto
+ * un telefono è contraffatop se il suo sistema operativo è iOs e la sua marca NON è Apple
+ * oppure se la marca è Apple e il sistema operativo NON è iOs
+ */
+public class Smartphone 
+{
     
 }
--- src/com/generation/es4/SmartphoneAvvio.java
+++ src/com/generation/es4/SmartphoneAvvio.java
@@ -4,5 +4,14 @@ public class SmartphoneAvvio
 {
     public static void main(String[] args) {
         
+        //Classico programma come quelli sopra
+        //Questa volta le variabili di accumulazione sono:
+        //numeroTelefoni android
+        //numeroTelefoni iOs
+        //prezzo medio android
+        //prezzo medio iOs
+        //numero telefoni contraffatti
+        //soldi persi (prezzo totale telefoni contraffatti)
+
     }    
 }
